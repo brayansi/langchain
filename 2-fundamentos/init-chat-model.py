e75440e2be9bc0a26345661bf1d33b02cf51fb56
Brayan Santos <v.brayansantos@gmail.com>
2026-02-28 13:11:19 -0300

    Adiciona script init-chat-model.py para inicializar e invocar o modelo "gemini-2.5-flash" utilizando LangChain.

---
+++ 2-fundamentos/init-chat-model.py
@@ -0,0 +1,11 @@
+from langchain.chat_models import init_chat_model
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = init_chat_model(model="gemini-2.5-flash", model_provider="google_genai")
+
+message = model.invoke("Hello, world!")
+
+print(message.content)
